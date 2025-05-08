4104d053ec9164ccd356d6d5c07bace191dd08cd
Anastasiia Bielik <ana.stasy0112@gmail.com>
2025-05-09 00:52:38 +0300

    -deleted comments

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,54 +1,69 @@
 import createElement from '../helpers/domHelper';
 
 export function createFighterImage(fighter) {
-    const { source, name } = fighter;
-    const attributes = {
-        src: source,
-        title: name,
-        alt: name
-    };
-    const imgElement = createElement({
-        tagName: 'img',
-        className: 'fighter-preview___img',
-        attributes
-    });
+	const { source, name } = fighter;
+	const attributes = {
+		src: source,
+		title: name,
+		alt: name,
+	};
+	const imgElement = createElement({
+		tagName: 'img',
+		className: 'fighter-preview___img',
+		attributes,
+	});
 
-    return imgElement;
+	return imgElement;
 }
 
 export function createFighterPreview(fighter, position) {
-    if (!fighter) {
-        const emptyElement = createElement({
-            tagName: 'div',
-            className: 'fighter-preview___empty'
-        });
-        return emptyElement;
-    }
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
+	if (!fighter) {
+		const emptyElement = createElement({
+			tagName: 'div',
+			className: 'fighter-preview___empty',
+		});
+		return emptyElement;
+	}
+	const positionClassName =
+		position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+	const fighterElement = createElement({
+		tagName: 'div',
+		className: `fighter-preview___root ${positionClassName}`,
+	});
 
-    // todo: show fighter info (image, name, health, etc.)
-    const fighterImage = createFighterImage(fighter);
+	const fighterImage = createFighterImage(fighter);
 
-    const name = createElement({ tagName: 'h4', className: 'fighter-preview___name' });
-    name.innerText = fighter.name;
+	const name = createElement({
+		tagName: 'h4',
+		className: 'fighter-preview___name',
+	});
+	name.innerText = fighter.name;
 
-    const health = createElement({ tagName: 'p', className: 'fighter-preview___stat' });
-    health.innerText = `Health: ${fighter.health}`;
+	const health = createElement({
+		tagName: 'p',
+		className: 'fighter-preview___stat',
+	});
+	health.innerText = `Health: ${fighter.health}`;
 
-    const attack = createElement({ tagName: 'p', className: 'fighter-preview___stat' });
-    attack.innerText = `Attack: ${fighter.attack}`;
+	const attack = createElement({
+		tagName: 'p',
+		className: 'fighter-preview___stat',
+	});
+	attack.innerText = `Attack: ${fighter.attack}`;
 
-    const defense = createElement({ tagName: 'p', className: 'fighter-preview___stat' });
-    defense.innerText = `Defense: ${fighter.defense}`;
+	const defense = createElement({
+		tagName: 'p',
+		className: 'fighter-preview___stat',
+	});
+	defense.innerText = `Defense: ${fighter.defense}`;
 
-    const infoContainer = createElement({ tagName: 'div', className: 'fighter-preview___info' });
-    infoContainer.append(name, health, attack, defense);
+	const infoContainer = createElement({
+		tagName: 'div',
+		className: 'fighter-preview___info',
+	});
+	infoContainer.append(name, health, attack, defense);
 
-    fighterElement.append(fighterImage, infoContainer);
+	fighterElement.append(fighterImage, infoContainer);
 
-    return fighterElement;
+	return fighterElement;
 }
